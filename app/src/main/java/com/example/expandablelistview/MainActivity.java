606d29e443112db8b60653c3a9b214d8f8321712
caoguoan <edison-c@foxmail.com>
2019-07-27 21:36:57 +0800

    fixed small bugs

--- app/src/main/java/com/example/expandablelistview/MainActivity.java
+++ app/src/main/java/com/example/expandablelistview/MainActivity.java
@@ -1,11 +1,12 @@
-package com.journaldev.expandablelistview;
+package com.example.expandablelistview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
-        expandableListDetail = ExpandableListDataPump.getData();
+        expandableListDetail = com.journaldev.expandablelistview.ExpandableListDataPump.getData();
         expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
         expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
         expandableListView.setAdapter(expandableListAdapter);
